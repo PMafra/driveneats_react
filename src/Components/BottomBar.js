9a9f0ab8cc8ce6e2d1558f4c950929e028519e84
PMafra <pmafrarm@gmail.com>
2021-08-28 18:57:26 -0300

    Button message now changing; css for button message added

--- src/Components/BottomBar.js
+++ src/Components/BottomBar.js
@@ -1,8 +1,16 @@
 export default function BottomBar ({changeColor}) {
+
+    let buttonMessage = "";
+    if (changeColor === "green-button") {
+        buttonMessage = "Fechar pedido";
+    } else {
+        buttonMessage = "Selecione pelo menos 1 item de cada categoria para fechar o pedido";
+    }
+
     return (
         <div class="bottom-bar">
             <button class={`bottom-bar-button ${changeColor}`} onclick="confirm()">
-                <p class="font-weight-400">Selecione pelo menos 1 item de cada<br /> categoria para fechar o pedido</p>
+                <p class="font-weight-400">{buttonMessage}</p>
             </button>
         </div>
     );
